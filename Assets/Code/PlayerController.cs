7317aeebc30578ec92ee979463cc3879bbce7c88
koweechiii <288015985+koweechiii@users.noreply.github.com>
2026-05-30 18:35:26 +0900

    FIRST DEBUG LOG

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,12 +6,39 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     
+        Printerist("fml pls");
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+   void Printerist(string expectedstring)
+    {
+       
+        string receivedstring = expectedstring;
+
+        
+        Debug.Log(receivedstring);
+
+
+
+      
+        
+
+
+    }
+
+        
+
+
+
+
+
+
+
 }
